d03c4c5832a7fc355110c4aaad214da58145a644
Vidit Singhal <vs7342@g.rit.edu>
2022-03-24 16:18:32 +0530

    Adding env config files. Implementing dotenv to determine environent and connect to mongodb.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const mongoose = require('mongoose');
 const app = express();
+
+// Determine current environment based on 'CURRENT' environment variable
+// CURRENT defaults to dev. Possible values - dev,prod
+const current_env = process.env.CURRENT || 'dev';
+
+// Import environment variables from the corresponding environment file
+require('dotenv').config({path: "./config/" + current_env + ".env"});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// MongoDB Configuration
+mongoose.connect(proces.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+// Home page
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
